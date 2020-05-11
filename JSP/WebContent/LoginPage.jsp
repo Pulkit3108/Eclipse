147b6076aca0897f950a69100a2d85e37a611e4c
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-05-11 13:24:10 +0530

    Add files via upload

---
+++ JSP/WebContent/LoginPage.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login JSP Page</title>
+</head>
+<body>
+	<form action=login method="post">
+		Please enter username and password:<br> Username: <input
+			type="text" name="username"><br> Password <input
+			type="text" name="password"><br> <input type="submit">
+	</form>
+</body>
+</html>
---
+++ JSP/src/com/PM/LoginServlet.java
@@ -0,0 +1,25 @@
+package com.PM;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		if (username.isEmpty() || password.isEmpty()) {
+			res.sendRedirect("LoginPage.jsp?");
+		} else {
+			res.sendRedirect("WelcomePage.jsp?user=" + username);
+		}
+
+	}
+	
+}
